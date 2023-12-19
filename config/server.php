e9daea4d793016531072e3a0eec3ab03d2f6e883
Aisha Muhammad Aliyu <ashmah017@gmail.com>
2023-12-19 04:12:23 +0100

    second commit

---
+++ config/server.php
@@ -0,0 +1,16 @@
+<?php
+
+    require "constant.php";
+$sname = "localhost";
+$uname = "root";
+$pass = "";
+$dbname = "dataweb";
+
+    $con = new mysqli (DB_HOST, DB_USER,DB_PASS, DB_NAME);
+
+    // if($con){
+    //     echo "connectted";
+    // }
+
+ 
+?>
---
+++ land.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./lineawesome/css/line-awesome.min.css">
+    <title>landing page</title>
+    <link rel="stylesheet" href="./land.css">
+</head>
+<body>
+    <div class="container">
+        <header></header>
+
+                <div class="cont"></div>
+                <div class="cont1"></div>
+                <div class="cont2"></div>
+                <div class="cont3"></div>
+
+            <div class="sign">
+                <div class="logo">
+                    <img src="./4ahi 1.png" width="60px" alt="">
+                </div>
+                <a href="login.php">
+                    <h4>SIGN IN</h4>
+                </a>
+            </div>
+            
+            <div class="slogan">
+                <h1>The key to success</h1><br>
+                <h1>is in your data!</h1>
+            </div>
+
+            <div class="note">
+                <h5></h5>
+            </div>
+            
+            <div class="btn">
+              <a href="register.html"><button class="btnn">Get started</button></a>
+            </div>
+
+            <div class="mid">
+                <label for="">Choose Network</label>
+                <input type="text">
+
+                <label for="">Select Plan</label>
+                <input type="text">
+
+                <label for="">Phone Number</label>
+                <input type="text">
+
+                <label for="">Email</label>
+                <input type="text">
+
+                <button class="but">Pay</button>
+
+            </div>
+            <footer>
+                <div class="foo">
+                    <span>Services</span> <br> <br>
+                    <h4> We offer instant recharge of Airtime,</h4> <br>
+                    <h4>data bundle,Cable TV( DSTv, GOTv, and Startimes)</h4> 
+                    </h4>
+                </div>
+                <div class="ter"> 
+                    <h3>
+                        <span>Contact Us</span> <br> <br>
+                        <h4>for complaint,feedback or unresolved issues</h6>
+                    </h3> 
+                 </div>
+
+            <div class="contact">
+                <i class="lab la-facebook-f"><span> 4Ahi Enterprises.com</span></i>
+                <i class="lab la-instagram"><span> 4Ahi_Enterprises</span></i>
+                <i class="lab la-twitter"><span> 4Ahi_Enterprises</span></i>
+            </div>
+
+            </footer>
+    </div>
+
+</body>
+</html>
